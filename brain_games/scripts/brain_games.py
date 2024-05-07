eadedd63003ad1a509e25fd058818721173943af
Artem Serov <serov4rtem@gmail.com>
2024-05-07 23:14:13 +0300

    end 2

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-print('poetry run python -m brain_games.scripts.brain_games')
-print('Welcome to the Brain Games!')
+
+def main():
+    print('Welcome to the Brain Games!')
+
 if __name__ == '__main__':
     main()
