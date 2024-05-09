11e4a5ada6cec168b2349be0a46e02bc431ee4c2
Artem Serov <serov4rtem@gmail.com>
2024-05-09 19:24:17 +0300

    General logic 6

---
+++ brain_games/all_logik_game.py
@@ -0,0 +1,34 @@
+def getRandom():
+    import random
+    return random.randint(0, 100)
+
+
+def checkAnswer(user_Name, user_Answer, right_Answer, counter):
+    i = counter
+    if user_Answer == right_Answer:
+        print('Correct!')
+        i += 1
+    else:
+        print(f"'{user_Answer}' is wrong answer ;(. Correct answer was '{right_Answer}'")
+        print(f"Let's try again, {user_Name}!")
+        i = 4
+    return i
+
+
+def Brain_Game(Game_Iter, game_Question):
+    import prompt
+    print(f"Welcome to the Brain Games!\n{game_Question}")
+    userName = prompt.string('May I have your name? ')  #
+    print(f"Hello, {userName}!\n")
+
+    i = 0
+    while i < 3:
+        question, right_Answer = Game_Iter()
+        userAnswer = input(f"Question: {question}\nYour answer ")
+        i = checkAnswer(userName, userAnswer, right_Answer, i)
+    if i < 4:
+        print(f"Congratulations, {userName}!")
+
+
+if __name__ == '__main__':
+    Brain_Game()
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from brain_games.even import brain_even
+from brain_games.games.even import brain_even
 
 
 def main():
